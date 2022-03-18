52674796c17df7ecd8960622ed36787921abee48
Jesus Cervantes <jescer3@gmail.com>
2022-03-17 20:37:57 -0700

    Added Jesus Cervantes to peer_printer.py

--- code/peer_printer.py
+++ code/peer_printer.py
@@ -50,12 +50,13 @@ def main():
     emrys_poggemann = Person('Emrys Poggemann', True)
     gary_kane = Person('Gary Kane', False)
     ian_schultz = Person('Ian Schultz', False)
+    jesus_cervantes = Person('Jesus Cervantes', True)
     joseph_doan = Person('Joseph Doan', True)
     nikita_cherepanov = Person('Nikita Cherepanov', True)
     vaibhav_jain = Person('Vaibhav Jain', False)
 
     # List of people in our class (alphabetical order by first name)
-    peers = [ashley_palencia_wisniewski, brandon_symansiv, darryl_nguyen, emrys_poggemann, gary_kane, ian_schultz, joseph_doan, nikita_cherepanov, vaibhav_jain]
+    peers = [ashley_palencia_wisniewski, brandon_symansiv, darryl_nguyen, emrys_poggemann, gary_kane, ian_schultz,jesus_cervantes, joseph_doan, nikita_cherepanov, vaibhav_jain]
 
     # Print out people in our class
     print("Welcome to learning Git in %s %s!" % (COURSE[0], SEMESTER[0]))
